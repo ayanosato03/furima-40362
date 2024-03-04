a5c14e2766f2f095510047a48095b045825ee45d
ayanosato03 <159245312+ayanosato03@users.noreply.github.com>
2024-03-04 14:36:16 +0900

    ユーザーの新規登録

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_03_03_071539) do
+ActiveRecord::Schema[7.0].define(version: 2024_03_04_052133) do
   create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "nickname", null: false
     t.string "email", default: "", null: false
@@ -19,7 +19,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_03_03_071539) do
     t.string "first_name", null: false
     t.string "kana_family_name", null: false
     t.string "kana_first_name", null: false
-    t.string "birthday", null: false
+    t.date "birthday", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
