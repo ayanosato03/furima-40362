cc53022903caddc946c68ffc40f3cbf159678161
ayanosato03 <159245312+ayanosato03@users.noreply.github.com>
2024-03-05 19:38:56 +0900

    DB修正
    birthdayカラムの型変更

--- db/migrate/20240303071539_devise_create_users.rb
+++ db/migrate/20240303071539_devise_create_users.rb
@@ -11,7 +11,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
       t.string :first_name,         null: false
       t.string :kana_family_name,   null: false
       t.string :kana_first_name,    null: false
-      t.string :birthday,           null: false
+      t.date :birthday,           null: false
 
       ## Recoverable
       t.string   :reset_password_token
