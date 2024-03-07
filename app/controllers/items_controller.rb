afdc5a9332137ab7957f1aabf605566550132f79
ayanosato03 <159245312+ayanosato03@users.noreply.github.com>
2024-03-07 20:16:10 +0900

    Rubocop実行

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,28 +1,27 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   def index
-   @items = Item.all.includes(:user)
+    @items = Item.all.includes(:user)
   end
 
   def new
-   @item = Item.new
+    @item = Item.new
   end
 
   def create
-   @item= current_user.items.build(item_params) #新規のPrototypeインスタンスを作成
-  if @item.save #該当のインスタンスを保存
-   redirect_to '/'
-  else
-    puts @item.errors.full_messages
-    render :new, status: :unprocessable_entity
-   end    
+    @item = current_user.items.build(item_params) # 新規のPrototypeインスタンスを作成
+    if @item.save # 該当のインスタンスを保存
+      redirect_to '/'
+    else
+      puts @item.errors.full_messages
+      render :new, status: :unprocessable_entity
+    end
   end
-
 
   private
+
   def item_params
-    params.require(:item).permit(:name, :description, :category_id, :condition_id, :shipping_fee_id, :prefecture_id, :shipping_day_id, :price,:image).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :description, :category_id, :condition_id, :shipping_fee_id, :prefecture_id,
+                                 :shipping_day_id, :price, :image).merge(user_id: current_user.id)
   end
-
-
 end
