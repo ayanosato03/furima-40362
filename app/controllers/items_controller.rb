ed6062be302e66370fdbdd7e5751ec029a094df5
ayanosato03 <159245312+ayanosato03@users.noreply.github.com>
2024-03-10 19:20:23 +0900

    ログインの有無で商品情報編集ページに推移できるかを変える

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,7 +23,7 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
-
+    redirect_to root_path unless current_user.id == @item.user.id
   end
 
   def update
