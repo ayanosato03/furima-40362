391b7e8173663e0b9088b5279e57034463adb028
ayanosato03 <159245312+ayanosato03@users.noreply.github.com>
2024-03-12 15:46:50 +0900

    購入フォーム情報保存機能実装

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,10 +1,10 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :municipalities, :street_address, :building_name, :phone_number, :user_id,:item_id
+  attr_accessor :post_code, :prefecture_id, :municipalities, :street_address, :building_name, :phone_number, :user_id,:item_id
  
  with_options presence: true do
-  validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/, message: '郵便番号は3桁-4桁の形式で入力してください' }
-  validates :prefecture_id, presence: { message: '都道府県を入力してください' }
+  validates :post_code, format: { with: /\A\d{3}-\d{4}\z/, message: '郵便番号は3桁-4桁の形式で入力してください' }
+  validates :prefecture_id, presence: true, numericality: { other_than: 1, message: '都道府県を入力してください' }
   validates :municipalities, presence: { message: '市区町村を入力してください' }
   validates :street_address, presence: { message: '番地を入力してください' }
   validates :building_name
@@ -14,8 +14,8 @@ class OrderAddress
  end
 
  def save
-  order = Donation.create(item_id: item_id, user_id: user_id)
-  Address.create(postal_code: postal_code, prefecture_id: prefecture_id, municipalities: municipalities, street_address: street_address, building_name: building_name, phone_number:phone_number, order_id: order.id)
+  order = Order.create(item_id: item_id, user_id: user_id)
+  Address.create(post_code: post_code, prefecture_id: prefecture_id, municipalities: municipalities, street_address: street_address, building_name: building_name, phone_number:phone_number, order_id: order.id)
 end
 
 end
