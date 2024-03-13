55840228cf450452f8a67b733e475801df294006
ayanosato03 <159245312+ayanosato03@users.noreply.github.com>
2024-03-13 19:13:35 +0900

    ビュー追記
    商品一覧ページ
    商品詳細ページ

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,7 +23,11 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    redirect_to root_path unless current_user.id == @item.user.id
+    @item = Item.find(params[:id])
+    if current_user.id == @item.user_id && @item.order.blank?
+    else
+      redirect_to root_path
+    end
   end
 
   def update
